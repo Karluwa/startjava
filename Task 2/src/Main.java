baddfa50c4ebb3f918b9fe1d8a2f6a07a411b96f
Karluwa <kkarlina@mail.ru>
2023-05-06 06:55:56 +0300

    done

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
+
+//               for (int i =1; i <= 100; i++)
+//                {if (i % 2 == 0) {continue;}
+//            System.out.println("Уволен айтишник с id " + i);
+//        }
+        int i = 1;
+        while (i <= 100) {
+            i +=2;
+                       System.out.println("Уволен айтишник с id " + i);
+        }
     }
 }
