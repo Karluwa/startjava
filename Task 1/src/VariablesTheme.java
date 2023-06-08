3d76b1b46b9cd532bae6fd10418734f2c46f5432
Karluwa <kkarlina@mail.ru>
2023-06-08 09:14:44 +0300

    done

---
+++ Task 1/src/VariablesTheme.java
@@ -0,0 +1,20 @@
+public class VariablesTheme {
+    public static void main(String[] args) {
+        byte ozu = 4;
+        int dischargeProsessor = 64;
+        long codeOfProduct = 1794041729;
+        float prosessorFrequency = 1.6f;
+        double screen = 15.6;
+        boolean goodFirma = true;
+        char releaseWindows = '8';
+        String computerName = "VAIO";
+        System.out.println("Установленная память, Гб " + ozu);
+        System.out.println("Количество разрядов процессора " + dischargeProsessor);
+        System.out.println("Код продукта " + codeOfProduct);
+        System.out.println("Частота процессора, ГГц " + prosessorFrequency);
+        System.out.println("Размер экрана в дюймах " + screen);
+        System.out.println("Ноутбук хорошей фирмы? " + goodFirma);
+        System.out.println("Выпуск Windows " + releaseWindows);
+        System.out.println("Имя компьютера " + computerName);
+    }
+}
