285e39aec8bf68a631f1dd58519b1dab5c5a0eee
Karluwa <kkarlina@mail.ru>
2023-06-29 20:30:22 +0300

    done Var

--- Task 1/src/VariablesTheme.java
+++ Task 1/src/VariablesTheme.java
@@ -2,7 +2,7 @@ public class VariablesTheme {
     public static void main(String[] args) {
         byte ozu = 4;
         int dischargeProsessor = 64;
-        long codeOfProduct = 1794041729;
+        long codeOfProduct = 1794041729l;
         float prosessorFrequency = 1.6f;
         double screen = 15.6;
         boolean goodFirma = true;
@@ -15,6 +15,100 @@ public class VariablesTheme {
         System.out.println("Размер экрана в дюймах " + screen);
         System.out.println("Ноутбук хорошей фирмы? " + goodFirma);
         System.out.println("Выпуск Windows " + releaseWindows);
-        System.out.println("Имя компьютера " + computerName);
+        System.out.println("Имя компьютера " + computerName + "\n");
+
+        int priceOfPen = 100;
+        int priceOfBook = 200;
+        double discount = 0.11;
+        int totalPriceWithoutDiscount = priceOfPen + priceOfBook;
+        double PriceOfDiscount = (priceOfPen + priceOfBook) * discount;
+        double totalPriceWithDiscount = (priceOfPen + priceOfBook) - PriceOfDiscount;
+        System.out.println("Общая цена товаров без скидки, руб. " + totalPriceWithoutDiscount);
+        System.out.println("Сумма скидки, руб. " + PriceOfDiscount);
+        System.out.println("Общая цена товвров со скидкой, руб. " + totalPriceWithDiscount + "\n");
+
+        System.out.println("\t\t J\t" + "\t   a\t v\t\t\t v\t a");
+        System.out.println("\t\t J" + "\t\t a  a\t  v" + "\t\t   v " + "  a " + "  a");
+        System.out.println("J\t\t J\t" + "\t aaaaa\t   " + "V   V " + "  aaaaa" + "\n");
+
+        byte maxBite = 127;
+        short maxShort = 32767;
+        int maxInt = 2147483647;
+        long maxLong = 9223372036854775807l;
+        System.out.println("первоначальное значение maxByte " + maxBite + "\nзначение maxByte после инкремента на единицу "
+                + maxBite++ + "\nзначение maxByte после декремента на единицу " + maxBite--+ "\n");
+        System.out.println("первоначальное значение maxShort " + maxShort + "\nзначение maxShort после инкремента на единицу "
+                + maxShort++ + "\nзначение maxShort после декремента на единицу " + maxShort--+ "\n");
+        System.out.println("первоначальное значение maxInt " + maxInt + "\nзначение maxInt после инкремента на единицу "
+                + maxInt++ + "\nзначение maxInt после декремента на единицу " + maxInt--+ "\n");
+        System.out.println("первоначальное значение maxLong " + maxLong + "\nзначение maxLong после инкремента на единицу "
+                + maxLong++ + "\nзначение maxLong после декремента на единицу " + maxLong--+ "\n");
+
+        int a = 2;
+        int b = 5;
+        int tmp;
+        System.out.println("Перестановка значений переменных с помощью третьей переменной:");
+        System.out.println("Исходное значения переменной а: " + a + "\tИсходное значения переменной b: " + b);
+        tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println("Новое значения переменной а: " + a + "\tНовое значения переменной b: " + b);
+        System.out.println("Перестановка значений переменных с арифметической операции:");
+        System.out.println("Исходное значения переменной а: " + a + "\tИсходное значения переменной b: " + b);
+        b = a + b;
+        a = b - a;
+        b = b - a;
+        System.out.println("Новое значения переменной а: " + a + "\tНовое значения переменной b: " + b);
+        System.out.println("Перестановка значений переменных с помощью битовой манипуляции:");
+        System.out.println("Исходное значения переменной а: " + a + "\tИсходное значения переменной b: " + b);
+        b = a ^ b;
+        a = b ^ a;
+        b = b ^ a;
+        System.out.println("Новое значения переменной а: " + a + "\tНовое значения переменной b: " + b);
+
+        System.out.println("Вывод символов и их кодов");
+        char c1 = 35;
+        int aC1 = c1;
+        System.out.println("символ " + c1 + " имеет код ASCII " + aC1);
+        char a1 = 38;
+        int aA1 = a1;
+        System.out.println("символ " + a1 + " имеет код ASCII " + aA1);
+        char b1 = 64;
+        int aB1 = b1;
+        System.out.println("символ " + b1 + " имеет код ASCII " + aB1);
+        char d1 = 94;
+        int aD1 = d1;
+        System.out.println("символ " + d1 + " имеет код ASCII " + aD1);
+        char e1 = 95;
+        int aE1 = c1;
+        System.out.println("символ " + e1 + " имеет код ASCII " + aE1);
+
+        String s = "\\";
+        String q = "/";
+        String t = "_";
+        String u = "(";
+        String v = ")";
+        System.out.println("\n\t\t " + q + " " + s);
+        System.out.println("\t\t  " + q + "\t " + s);
+        System.out.println("\t\t " + q + t + u + v + s);
+
+        int num1 = 123;
+        int num2 = num1/100;
+        int num3 = num1%100/10;
+        int num4 = num1%10;
+        System.out.println("Число " + num1 + " содержит: " + num2 + " сотен " + num3 + " десятков " + num4 + " единиц");
+        int num5 = num2 + num3 + num4;
+        int num6 = num2 * num3 * num4;
+        System.out.println("Сумма его цифр = " + num5 + " Произведение = " + num6);
+
+        System.out.println("\t\tВывод времени:");
+        int time = 86399;
+        int hour = time/360;
+        int min = time%360/60;
+        int sec = min%60;
+        System.out.println("\t\tЧЧ:" + hour + " ММ:" + min + " СС:" + sec);
+
+
+
     }
 }
